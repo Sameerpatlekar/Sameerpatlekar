36b23ecc3473fa788945f620d9fb7122cc193ab0
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-05-31 23:02:30 +0530

    f

--- terraform/ec2-instace.tf
+++ terraform/ec2-instace.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with the desired AMI ID
+  ami           = "ami-00beae93a2d981137"  # Replace with the desired AMI ID
   instance_type = "t2.micro"
 
   tags = {
