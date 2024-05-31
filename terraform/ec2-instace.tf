099ff27927b08c6cb120e72ede99f128cde9908a
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-05-31 23:24:19 +0530

    gh

--- terraform/ec2-instace.tf
+++ terraform/ec2-instace.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "my-terraform-Instance"
   }
 
   # Optional: Define a key pair for SSH access
