5161f996e58a4e00f8d66c2e02da0fc16051e255
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-03 13:10:11 +0530

    fg

---
+++ terraform/blocktype-terraform/terraform.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tfstate-bucket"
+
+  tags = {
+    Name = "my-tfstate-bucket"
+    Env = "Dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-tfstate-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow SSH for all network"
+  vpc_id      = "vpc-088581bab111749bc" # VPC ID should be a string
+
+  ingress {
+    description = "SSH rule"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+    Env  = "Dev"
+  }
+}
