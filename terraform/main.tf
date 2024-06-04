fa196f31459d14110525ed7c49a1268107ec1302
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-04 13:35:05 +0530

    fh

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,17 +22,18 @@ module "vpc" {
 
 module "ec2" {
     source = "./module/ec2"
-    this_aws_instance_ami = "ami-0eb5115914ccc4bc2"
+    this_aws_instance_ami = "ami-04b70fa74e45c3917"
     this_aws_instance_instance_type  = "t2.micro"
     //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
-    this_aws_instance_key_name = "delete_oregon_anup"
+    this_aws_instance_key_name = "ec2-key"
     this_aws_instance_availability_zone = "us-east-1a"
     this_aws_instance_volume_size = 8
     this_aws_instance_count = 2
     this_aws_instance_user_data_base64 = true
     this_aws_instance_tags =  "Terraform_instance"
     this_aws_instance_subnet = module.vpc.subnet_id
-    this_sg_vpc_id = module.vpc.vpc_id
-    //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
+    #this_sg_vpc_id = module.vpc.vpc_id
+    this_aws_vpc_id = module.aws_vpc.this_vpc.id
+    
 }
 
---
+++ terraform/module/ec2/variable.tf
@@ -0,0 +1,43 @@
+variable "this_aws_instance_ami" {
+     type = string
+}
+
+variable "this_aws_instance_instance_type" {
+     type = string
+}
+
+variable "this_aws_instance_key_name" {
+     type = string
+}
+
+variable "this_aws_instance_availability_zone" {
+     type = string
+}
+
+variable "this_aws_instance_volume_size" {
+     type = string
+}
+
+variable "this_aws_instance_count" {
+     type = string
+}
+
+variable "this_aws_instance_user_data_base64" {
+     type = string
+}
+
+variable "this_aws_instance_tags" {
+     type = string
+}
+
+variable "this_aws_instance_subnet" {
+     type = string
+}
+
+variable "this_sg_vpc_id" {
+     type = string
+}
+
+variable "this_aws_vpc_id" {
+     type = string
+}
